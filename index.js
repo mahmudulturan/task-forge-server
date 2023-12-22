9507ce4621d154f283d87144f4a23cb64fb54afe
Mahmudul Hasan <97558692+mahmudulturan@users.noreply.github.com>
2023-12-22 19:38:41 +0600

    implement delete endpoint to delete a task

--- index.js
+++ index.js
@@ -79,7 +79,14 @@ async function run() {
         })
 
 
-
+         //to delete a Task 
+        app.delete('/delete-task/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await tasksCollection.deleteOne(query);
+            res.send(result);
+        })
+        
 
 
         // Send a ping to confirm a successful connection
